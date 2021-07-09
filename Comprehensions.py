48768108757eb2eed161300591c2890bf83f6986
juliana1thome <juliana1thome@gmail.com>
2021-07-09 19:28:07 -0400

    Update List Comprehension

--- Comprehensions.py
+++ Comprehensions.py
@@ -52,10 +52,33 @@ gmovies2 = [title for title in movies if title.startswith("G")]
 print(gmovies2)
 print("*********************************************************")
 
-# Example4 ( with if clause):
-# Get the movies that were released before 200 and ends with letter a
+# Example4 (with if clause):
+# Get the movies that were released before 200 and ends with letter a.
 movies = [("Star Wars: The Phantom Menace", 1999), ("Gandhi", 1982), ("Cassablanca", 1942), ("Citizen Kane", 1941), ("Thor", 2011), ("Ghostbusters", 1984), ("Good Will Hunting", 1997), ("Close Encounters of the Third Kind", 1977), ("Sharknado", 2013), ("Thor2", 2013), ("Gattaca", 1997), ("Pippi Longstocking", 1969), ("Mary Poppins", 1964)]
 pre2000 = [(title, year) for (title, year) in movies if year <= 2000 if title.endswith('a')]
 print(pre2000)
 print("*********************************************************")
+
+# Example5:
+# Multiplicate each element of the list using List Comprehension
+x = [2, -3, 4]
+y = [4 * element for element in x]
+print(y)
+print("*********************************************************")
+
+# Example6:
+# If A and B are sets, then the Cartesian product is the set of pairs (a, b) where 'a' is in A and 'b' is in B.
+# A = {1, 2}
+# B = {3, 4}
+# A * B = {(1, 3),(1, 4),(2, 3),(2, 4)}
+# Therefore, use List Comprehension to obtain the cartesian product of 2 sets,
+
+A = [1, 3, 5, 7]
+B = [2, 4, 6, 8]
+
+cartesian_product = [(A_element, B_element) for A_element in A for B_element in B]
+print(cartesian_product)
+print("*********************************************************")
+
+
 
